ddd514e65f26f58a6b6d08ea4fba93f17b83e947
Maryam <maryamch1423@gmail.com>
2026-06-14 19:15:03 +0330

    Add function to calculator module and refactor it

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,6 +1,13 @@
 #importing operators to caculating
 from operators import plus_operator, minus_operator, time_operator, divided_operator
 
+def check_quit(user_input):
+    """Check if user wants to quit"""
+    if user_input.strip().lower() == 'q':
+        print("\n👋 Have a good day!")
+        return True
+    return False
+
 print("This is a simple calculator. If you want to quit, enter 'q'.")
 
 operators = ['+', '-', '*', '/']    #define list of operators for checking entry operator
@@ -9,40 +16,34 @@ while True:
     number1 = input('\nEnter first number: ')   #getting first number
        
     #checking if user wants to quit
-    if number1.strip().lower() == 'q':      
-        print("\nHave a good day!")
+    if check_quit(number1):
         break
 
     operator = input('Enter operator(+, -, *, /): ')    #getting operator
     
     #check if user wants to quit
-    if operator.strip().lower() == 'q':
-        print("\nHave a good day!")
+    if check_quit(operator):
         break
 
     number2 = input('Enter second number: ')    #gettin second number
     
     #check if user wants to quit
-    if number2.strip().lower() == 'q':
-        print("\nHave a good day!")
+    if check_quit(number2):
         break
     
     #check operator to calculating
     if operator.strip() in operators:
         if operator.strip() == '+':
-            answer = plus_operator(number1, number2)
+            plus_operator(number1, number2)
         elif operator.strip() == '-':
-            answer = minus_operator(number1, number2)
+            minus_operator(number1, number2)
         elif operator.strip() == '*':
-            answer = time_operator(number1, number2)
+            time_operator(number1, number2)
         elif operator.strip() == '/':
-            answer = divided_operator(number1, number2)
+            divided_operator(number1, number2)
     
     #check if user doesn't enter any operator
     elif operator.strip() not in operators:
         print("\nPlease enter valid operator!")
     else:
         print("\nPlease enter valid operator!")
-
-        
-    
